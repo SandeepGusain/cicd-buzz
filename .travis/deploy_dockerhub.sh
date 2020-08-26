76f6f42ad09d09c4541443d42ddcaa8808ae1a46
Sandeep Gusain <crazyguyx4@gmail.com>
2020-08-26 12:51:45 +0530

    Substitute username with the correct one

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,7 +5,13 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-CORRECT_NAME=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]')
+
+#convert name to lower-case
+#CORRECT_NAME=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]')
+
+IFS='/' read -ra GITHUB_USER <<< "$TRAVIS_REPO_SLUG"
+CORRECT_NAME=${TRAVIS_REPO_SLUG/$GITHUB_USER/$DOCKER_USER}
+
 docker build -f Dockerfile -t $CORRECT_NAME:$TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 sudo docker push $CORRECT_NAME:$TAG
